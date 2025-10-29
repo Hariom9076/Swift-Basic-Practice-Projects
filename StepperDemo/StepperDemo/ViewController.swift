19bab513c4bfe4f43b330b488a79b66559341136
Hariom9076 <hariomchauraisya@gmail.com>
2025-10-29 12:54:03 +0530

    stepper project done

---
+++ StepperDemo/StepperDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StepperDemo
+//
+//  Created by Hariom Chaurasiya on 29/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func valueChanged(_ sender: UIStepper) {
+        label.text = Int(sender.value).description
+    }
+    
+    
+
+}
+
