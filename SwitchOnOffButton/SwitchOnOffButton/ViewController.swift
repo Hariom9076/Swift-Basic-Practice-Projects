9cacb5b5c6320297d88184ba3875d6d234a2ef9f
Hariom9076 <hariomchauraisya@gmail.com>
2025-10-29 12:27:33 +0530

    swift Projects

---
+++ SwitchOnOffButton/SwitchOnOffButton/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SwitchOnOffButton
+//
+//  Created by Hariom Chaurasiya on 29/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var switch1: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func valueChanged(_ sender: UISwitch) {
+        if switch1.isOn{
+            label.text = "switch is ON"
+            self.view.backgroundColor = UIColor.green
+        }else{
+            label.text = "switch is OFF"
+            self.view.backgroundColor = UIColor.red
+        }
+    }
+    
+}
+
